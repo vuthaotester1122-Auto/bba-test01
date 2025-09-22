4c79942e70e825093c01179fbd9414bb01e645ac
test 01 - ThaoVu <vuthaotester1122@gmail.com>
2025-09-22 22:24:37 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom-2.js
@@ -0,0 +1,19 @@
+const powerUp = "mushroom";
+if (powerUp === "mushroom"){
+    console.log("Mario becomes Super!")
+}
+else if (powerUp === "flower"){
+    console.log("Mario can shoot fireballs!")
+}
+
+else if (powerUp === "star"){
+    console.log("Mario is invincible!")
+}
+
+else if (powerUp === "none"){
+    console.log("Mario is normal")
+}
+
+else {
+    console.log("Unknown power-up")
+}
