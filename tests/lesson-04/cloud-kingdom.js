e3d22f2777905c1d0d9464121ff1fa4a6fe02882
test 01 - ThaoVu <vuthaotester1122@gmail.com>
2025-09-27 15:31:58 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,19 @@
+//Biến playerName có giá trị là “Mario”.
+const playerName = 'Mario';
+console.log(`${playerName}`);
+
+//Biến currentLives có giá trị là 3.
+const currentLives =  3;
+console.log(`${currentLives}`);
+
+// Tính giá trị trung bình coin của 3 level và in ra console
+const level1 = 25;
+const level2 = 30;
+const level3 = 45;
+const average = (level1 + level2 + level3) / 3;
+console.log(average);
+
+// In ra số coin dư khi chia cho 3
+console.log(level1 % 3);
+console.log(level2 % 3);
+console.log(level3 % 3);
